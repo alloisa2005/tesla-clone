767e5ea5ec9e5d0e86a3716c0227b6cb24754e72
Anthony Allois <anthonyallois@gmail.com>
2022-07-21 09:47:11 -0300

    Ajustes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,62 +1,74 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import MenuIcon from '@mui/icons-material/Menu';
-import CloseIcon from '@mui/icons-material/Close';
+import React, { useState } from "react";
+import styled from "styled-components";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import { selectCars } from "../features/car/carSlice";
+import { useSelector } from "react-redux";
 
 function Header() {
-
   const [open, setOpen] = useState(false);
+  const cars = useSelector(selectCars);
 
   return (
     <Container>
-        <a href="/">
-          <img src="./images/logo.svg" alt="Logo Tesla" />
-        </a>
-        <Menu>
-            <a href="/">Model S</a>
-            <a href="/">Model 3</a>
-            <a href="/">Model X</a>
-            <a href="/">Model Y</a>
-        </Menu>
+      <a href="/">
+        <img src="./images/logo.svg" alt="Logo Tesla" />
+      </a>
+      <Menu>
+        {
+          cars && cars.map( (car,index) => <a key={index} href="/">{car}</a>)
+        }
+      </Menu>
 
-        <RightMenu>
-          <a href="/">Shop</a>
-          <a href="/">Tesla Account</a>
-          <CustomMenu onClick={ () => setOpen(!open) } />
-        </RightMenu>
+      <RightMenu>
+        <a href="/">Shop</a>
+        <a href="/">Tesla Account</a>
+        <CustomMenu onClick={() => setOpen(!open)} />
+      </RightMenu>
 
-        <BurguerNav show={open}>           
-          <CloseWrapper>
-            <CustomClose onClick={ () => setOpen(!open) }/>                      
-          </CloseWrapper>
-          <li><a href="/">Existing Inventory</a></li>
-          <li><a href="/">Used Inventory</a></li>
-          <li><a href="/">Trade-in</a></li>
-          <li><a href="/">Cybertruck</a></li>
-          <li><a href="/">Roadaster</a></li>
-        </BurguerNav>
+      <BurguerNav show={open}>
+        <CloseWrapper>
+          <CustomClose onClick={() => setOpen(!open)} />
+        </CloseWrapper>
+        {
+          cars && cars.map( (car,index) => <li key={index}><a href="/">{car}</a></li>)
+        }
+        <li><a href="/">Existing Inventory</a></li>
+        <li>
+          <a href="/">Used Inventory</a>
+        </li>
+        <li>
+          <a href="/">Trade-in</a>
+        </li>
+        <li>
+          <a href="/">Cybertruck</a>
+        </li>
+        <li>
+          <a href="/">Roadaster</a>
+        </li>
+      </BurguerNav>
     </Container>
-  )
+  );
 }
 
-export default Header
+export default Header;
 
-const Container = styled.div`     
+const Container = styled.div`
   min-height: 60px;
   position: fixed;
   display: flex;
-  align-items: center;    
+  align-items: center;
   justify-content: space-between;
   padding: 0 20px;
   top: 0;
   left: 0;
   right: 0;
-  z-index: 10;  
-`
+  z-index: 10;
+`;
 
-const Menu = styled.div`  
+const Menu = styled.div`
   display: flex;
-  align-items: center;    
+  align-items: center;
   flex: 1;
   justify-content: center;
 
@@ -70,25 +82,24 @@ const Menu = styled.div`
   @media (max-width: 992px) {
     display: none;
   }
-`
+`;
 
 const RightMenu = styled.div`
   display: flex;
   align-items: center;
-  
+
   a {
     font-weight: 600;
     text-transform: uppercase;
-    margin-right: 10px;    
+    margin-right: 10px;
   }
-
-`
+`;
 
 const CustomMenu = styled(MenuIcon)`
   cursor: pointer;
-`
+`;
 
-const BurguerNav = styled.div`  
+const BurguerNav = styled.div`
   position: fixed;
   top: 0;
   bottom: 0;
@@ -101,23 +112,24 @@ const BurguerNav = styled.div`
   display: flex;
   flex-direction: column;
   text-align: start;
-  transform: ${props => props.show ? 'translateX(0)' : 'translateX(100%)'};
-  transition: transform 0.4s ease-in-out;
+  transform: ${(props) => (props.show ? "translateX(0)" : "translateX(100%)")};
+  transition: transform 0.4s;
+
   li {
     padding: 15px 0;
-    border-bottom: 1px solid rgba(0,0,0,.2);
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
 
     a {
       font-weight: 600;
     }
   }
-`
+`;
 
 const CloseWrapper = styled.div`
   display: flex;
   justify-content: flex-end;
-`
+`;
 
 const CustomClose = styled(CloseIcon)`
   cursor: pointer;
-`
+`;
